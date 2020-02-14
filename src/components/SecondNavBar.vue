4cd227228d3480f0a422401f2821f27624f12e84
Mike Schmitt <mike.schmitt3319@gmail.com>
2020-02-13 19:51:40 -0600

    Updated Second NavBar

--- src/components/SecondNavBar.vue
+++ src/components/SecondNavBar.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <md-toolbar>
-      <h3 style="flex: 1">Property Analysis</h3>
-      <div>
-        {{property.name ? property.name : property.address}}
+      <h3>Property Analysis</h3>
+      <div class="property">
+        <u>{{property.name ? property.name : property.address}}</u>
       </div>
-      <md-button class="md-raised md-primary">Share</md-button>
-      <md-button class="md-raised md-primary">Export</md-button>
+      <div class="md-toolbar-section-end">
+        <md-button class="button-outline">Share</md-button>
+        <md-button class="button-fill">Export</md-button>
+      </div>
     </md-toolbar>
   </div>
 </template>
@@ -23,4 +25,19 @@ export default class SecondNavBar extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .property {
+    margin-left: 20px;
+    font-size: 1.4rem;
+    color: #9D9D9D;
+  }
+  .button-fill {
+    background-color: #88B144;
+    color: #FFFFFF;
+    text-transform: capitalize;
+  }
+  .button-outline {
+    color: #88B144;
+    border:2px solid #88B144;
+    text-transform: capitalize;
+  }
 </style>
